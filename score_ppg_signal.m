73da3aa104407a2a7c7fb70145223ef4aae913e8
Eric Alfaro <ealfaro@mit.edu>
2025-04-28 08:52:26 -0400

    WIP scoring algorithm

--- score_ppg_signal.m
+++ score_ppg_signal.m
@@ -7,13 +7,18 @@ indices = split_ppg_signal(ppg_signal, sampling_frequency, cutoff_frequency);
 
 scores = zeros(length(indices), 1);
 
+
+
 for i=1:length(indices) - 1
     pulse = preprocess_ppg_pulse(ppg_signal(indices(i) : indices(i + 1)));
+    if (length(pulse) < 20) 
+        continue;
+    end
     [cos_coef, sin_coef] = calculate_fourier_coefficients(pulse);
-    scores(i) = mean(cos_coef(1:10));
+    scores(i) = sum(cos_coef(1:10)) + sum(sin_coef(1:10));
 end
 
-score = median(scores);
+score = median(scores);%median(maxk(scores, 3));
 
 end
 
--- test_ppg_score.m
+++ test_ppg_score.m
@@ -1,11 +1,23 @@
 sampling_frequency = 50; % Hertz
 cutoff_frequency = 1; % Hertz
+resampling_scale = 4;
 
-scores = zeros(length(files), 2);
+% Initialize the output
+scores = {[],[],[],[],[],[]};
 
-for i = 1:2
-    directory = uigetdir; 
+% Read the classification data
+table = readtable("data/patient_classification.csv");
+ids = table2array(table(:,1));
+labels = table2array(table(:,2));
+classification_lookup = dictionary(ids, labels);
+
+% Calculate scores for all PPG signals
+folders = ["data/young-athletic", "data/young-nonathletic", "data/good-nagpur"];
+for i = 1:3
+    directory = folders(i); 
     files = dir(fullfile(directory,'*.csv')); 
+
+    % Scan through all files in a folder
     for k = 1:length(files)
         base_name = files(k).name;
         file_name = fullfile(directory, base_name);
@@ -14,25 +26,65 @@ for i = 1:2
         
         % Resample the pulse to a higher rate to increase resolution,
         % using cubic spline interpolation
-        resampling_scale = 4;
         time = linspace(raw_ppg_signal(1, 1), ...
             raw_ppg_signal(length(raw_ppg_signal), 1), ...
             resampling_scale * length(raw_ppg_signal));
         ppg_signal = interp1(raw_ppg_signal(:, 1), raw_ppg_signal(:, 2), time, 'pchip');
-        
-        
-        scores(k, i) = score_ppg_signal(ppg_signal, ...
-            sampling_frequency * resampling_scale, ...
-            cutoff_frequency * resampling_scale);
-    end
+       
+        % Some signals are unusually short, discard those
+        if length(ppg_signal) < 5800
+            continue;
+        end
 
+        % Calculate the score
+        score = score_ppg_signal(ppg_signal, ...
+                                 sampling_frequency * resampling_scale, ...
+                                 cutoff_frequency * resampling_scale);
+        
+        if i <= 2
+            % Store young athletic/non-athletic in their own plots
+            scores{i}(end + 1) = score;
+        else 
+            % Split Nagpur data based on classification
+            id = str2double(base_name(1:3)); % Hacky way of getting the ID...
+            class = classification_lookup{id};
+
+            if strcmp(class, 'healthy') == 1 
+                index = 3;
+            elseif strcmp(class, 'hypertensive') == 1
+                index = 4;
+            elseif strcmp(class, 'pre CAD') == 1
+                index = 5;
+            elseif strcmp(class, 'CAD') == 1
+                index = 6;
+            end
+
+            scores{index}(end + 1) = score;
+        end
+    end
 end
+
+
+% Process data into a format that boxplot can use...
+% Strange and hacky
+y = num2cell(1:numel(scores));
+x = cellfun(@(x, y) [x(:) y*ones(size(x(:)))], scores, y, 'UniformOutput', 0);
+X = vertcat(x{:});
 
 hold on 
 
-boxplot(scores, {'Young Athletic','Young Nonathletic'})
+% Plot individual points
+spread = 0.125;
+x_center = 1:numel(scores); 
+for i = 1:numel(scores)
+    plot(rand(size(scores{i}))*spread -(spread/2) + x_center(i), scores{i}, '.', 'linewidth', 1)
+end
+
+boxplot(X(:,1), X(:,2))
+
 title('Cardiovascular Scores')
-xlabel('Group')
+key = {'Young Athletic', 'Young Nonathletic', 'Old Healthy', 'Old Hypertensive', 'Old Pre CAD', 'Old CAD'};
+xticklabels(key)
 ylabel('Score')
 
 hold off
